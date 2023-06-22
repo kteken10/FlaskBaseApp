82e943b34f2110576c1bed19cd0e6b4ada235c4d
DISSANG <dissangfrancis@yahoo.com>
2023-06-22 21:32:03 +0100

    API MAKE SUCESSFULY

--- backendApp/main.py
+++ backendApp/main.py
@@ -5,7 +5,7 @@ from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, session, jsonify, url_for,request
 from flask_cors import CORS
-from models import db, Product
+from models import db, Visiteur, Fournisseur, Automobile, AutomobileImage
 from config import config
 
 app = Flask(__name__)
@@ -59,79 +59,191 @@ def logout():
         )
     )
 
-@app.route("/users")
-def get_user_infos():
-    token = oauth.auth0.authorize_access_token()
-    session["user"] = token
-    user = session.get("user")
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({"message": "User not authenticated"})
-
-
-## GESTION DES UTILISATEUR
-
-# Route pour récupérer tous les produits
-@app.route('/api/products', methods=['GET'])
-def get_products():
-    products = Product.query.all()
-    result = []
-    for product in products:
-        product_data = {
-            'name': product.name,
-            'price': product.price
+# Routes pour les visiteurs
+@app.route('/visiteurs', methods=['GET'])
+def get_visiteurs():
+    visiteurs = Visiteur.query.all()
+    result = [
+        {
+            'id': visiteur.id,
+            'nom': visiteur.nom,
+            'email': visiteur.email,
+            'numero_telephone': visiteur.numero_telephone,
+            'photo_profil': visiteur.photo_profil,
+            'date_enregistrement': visiteur.date_enregistrement.isoformat()
         }
-        result.append(product_data)
+        for visiteur in visiteurs
+    ]
     return jsonify(result)
 
-# Route pour ajouter un produit
-@app.route('/api/products', methods=['POST'])
-def add_product():
-    data = request.get_json()
-    name = data.get('name')
-    price = data.get('price')
+@app.route('/visiteurs/<int:visiteur_id>', methods=['GET'])
+def get_visiteur(visiteur_id):
+    visiteur = Visiteur.query.get(visiteur_id)
+    if visiteur:
+        result = {
+            'id': visiteur.id,
+            'nom': visiteur.nom,
+            'email': visiteur.email,
+            'numero_telephone': visiteur.numero_telephone,
+            'photo_profil': visiteur.photo_profil,
+            'date_enregistrement': visiteur.date_enregistrement.isoformat()
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'message': 'Visiteur non trouvé'})
 
-    if not name or not price:
-        return jsonify({'error': 'Tous les champs sont obligatoires'}), 400
-
-    product = Product(name=name, price=price)
-    db.session.add(product)
+@app.route('/visiteurs', methods=['POST'])
+def create_visiteur():
+    data = request.json
+    visiteur = Visiteur(
+        nom=data['nom'],
+        email=data['email'],
+        numero_telephone=data['numero_telephone'],
+        photo_profil=data['photo_profil'],
+        date_enregistrement=data['date_enregistrement']
+    )
+    db.session.add(visiteur)
     db.session.commit()
+    return jsonify({'message': 'Visiteur créé avec succès'})
 
-    return jsonify({'message': 'Le produit a été ajouté avec succès'}), 201
+# Routes pour les fournisseurs
+@app.route('/fournisseurs', methods=['GET'])
+def get_fournisseurs():
+    fournisseurs = Fournisseur.query.all()
+    result = [
+        {
+            'id': fournisseur.id,
+            'nom_fournisseur': fournisseur.nom_fournisseur,
+            'email': fournisseur.email,
+            'numero_telephone': fournisseur.numero_telephone,
+            'logo_fournisseur': fournisseur.logo_fournisseur,
+            'date_enregistrement': fournisseur.date_enregistrement.isoformat(),
+            'localisation': fournisseur.localisation,
+            'adresse': fournisseur.adresse
+        }
+        for fournisseur in fournisseurs
+    ]
+    return jsonify(result)
 
-# Route pour mettre à jour un produit
-@app.route('/api/products/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
-    product = Product.query.get(product_id)
-    if not product:
-        return jsonify({'error': 'Produit non trouvé'}), 404
+@app.route('/fournisseurs/<int:fournisseur_id>', methods=['GET'])
+def get_fournisseur(fournisseur_id):
+    fournisseur = Fournisseur.query.get(fournisseur_id)
+    if fournisseur:
+        result = {
+            'id': fournisseur.id,
+            'nom_fournisseur': fournisseur.nom_fournisseur,
+            'email': fournisseur.email,
+            'numero_telephone': fournisseur.numero_telephone,
+            'logo_fournisseur': fournisseur.logo_fournisseur,
+            'date_enregistrement': fournisseur.date_enregistrement.isoformat(),
+            'localisation': fournisseur.localisation,
+            'adresse': fournisseur.adresse
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'message': 'Fournisseur non trouvé'})
 
-    data = request.get_json()
-    name = data.get('name')
-    price = data.get('price')
-
-    if not name or not price:
-        return jsonify({'error': 'Tous les champs sont obligatoires'}), 400
-
-    product.name = name
-    product.price = price
+@app.route('/fournisseurs', methods=['POST'])
+def create_fournisseur():
+    data = request.json
+    fournisseur = Fournisseur(
+        nom_fournisseur=data['nom_fournisseur'],
+        email=data['email'],
+        numero_telephone=data['numero_telephone'],
+        logo_fournisseur=data['logo_fournisseur'],
+        date_enregistrement=data['date_enregistrement'],
+        localisation=data['localisation'],
+        adresse=data['adresse']
+    )
+    db.session.add(fournisseur)
     db.session.commit()
+    return jsonify({'message': 'Fournisseur créé avec succès'})
 
-    return jsonify({'message': 'Le produit a été mis à jour avec succès'})
+# Routes pour les automobiles
+@app.route('/automobiles', methods=['GET'])
+def get_automobiles():
+    automobiles = Automobile.query.all()
+    result = [
+        {
+            'id': automobile.id,
+            'marque': automobile.marque,
+            'prix': float(automobile.prix),
+            'type_vehicule': automobile.type_vehicule,
+            'couleur': automobile.couleur,
+            'date_enregistrement': automobile.date_enregistrement.isoformat(),
+            'fournisseur_id': automobile.fournisseur_id
+        }
+        for automobile in automobiles
+    ]
+    return jsonify(result)
 
-# Route pour supprikmer un produit
-@app.route('/api/products/<int:product_id>', methods=['DELETE'])
-def delete_product(product_id):
-    product = Product.query.get(product_id)
-    if not product:
-        return jsonify({'error': 'Produit non trouvé'}), 404
+@app.route('/automobiles/<int:automobile_id>', methods=['GET'])
+def get_automobile(automobile_id):
+    automobile = Automobile.query.get(automobile_id)
+    if automobile:
+        result = {
+            'id': automobile.id,
+            'marque': automobile.marque,
+            'prix': float(automobile.prix),
+            'type_vehicule': automobile.type_vehicule,
+            'couleur': automobile.couleur,
+            'date_enregistrement': automobile.date_enregistrement.isoformat(),
+            'fournisseur_id': automobile.fournisseur_id
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'message': 'Automobile non trouvée'})
 
-    db.session.delete(product)
+@app.route('/automobiles', methods=['POST'])
+def create_automobile():
+    data = request.json
+    automobile = Automobile(
+        marque=data['marque'],
+        prix=data['prix'],
+        type_vehicule=data['type_vehicule'],
+        couleur=data['couleur'],
+        fournisseur_id=data['fournisseur_id']
+    )
+    db.session.add(automobile)
     db.session.commit()
+    return jsonify({'message': 'Automobile créée avec succès'})
 
-    return jsonify({'message': 'Le produit a été supprimé avec succès'})
+# Routes pour les images d'automobile
+@app.route('/images', methods=['GET'])
+def get_images():
+    images = AutomobileImage.query.all()
+    result = [
+        {
+            'id': image.id,
+            'url_image': image.url_image,
+            'automobile_id': image.automobile_id
+        }
+        for image in images
+    ]
+    return jsonify(result)
 
+@app.route('/images/<int:image_id>', methods=['GET'])
+def get_image(image_id):
+    image = AutomobileImage.query.get(image_id)
+    if image:
+        result = {
+            'id': image.id,
+            'url_image': image.url_image,
+            'automobile_id': image.automobile_id
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'message': 'Image non trouvée'})
+
+@app.route('/images', methods=['POST'])
+def create_image():
+    data = request.json
+    image = AutomobileImage(
+        url_image=data['url_image'],
+        automobile_id=data['automobile_id']
+    )
+    db.session.add(image)
+    db.session.commit()
+    return jsonify({'message': 'Image créée avec succès'})
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
--- backendApp/models.py
+++ backendApp/models.py
@@ -1,12 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+class Visiteur(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    numero_telephone = db.Column(db.String(255), nullable=False)
+    photo_profil = db.Column(db.String(255))
+    date_enregistrement = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+class Fournisseur(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_fournisseur = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    numero_telephone = db.Column(db.String(255), nullable=False)
+    logo_fournisseur = db.Column(db.String(255))
+    date_enregistrement = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    localisation = db.Column(db.String(255), nullable=False)
+    adresse = db.Column(db.String(255), nullable=False)
+
+class Automobile(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    marque = db.Column(db.String(255), nullable=False)
+    prix = db.Column(db.Numeric, nullable=False)
+    type_vehicule = db.Column(db.String(255), nullable=False)
+    couleur = db.Column(db.String(255), nullable=False)
+    date_enregistrement = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    fournisseur_id = db.Column(db.Integer, db.ForeignKey('fournisseur.id'))
+    fournisseur = db.relationship('Fournisseur', backref=db.backref('automobiles', lazy=True))
+
+class AutomobileImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    url_image = db.Column(db.String(255), nullable=False)
+    automobile_id = db.Column(db.Integer, db.ForeignKey('automobile.id'))
+    automobile = db.relationship('Automobile', backref=db.backref('images', lazy=True))
